bbe00297f0b0ccf6734f410fda0819c00ec225d9
c0linw <41310013+c0linw@users.noreply.github.com>
2022-01-19 14:41:55 -0800

    Add shader cache to Game scene to prevent initial lag

---
+++ Scenes/game/shader_cache.gd
@@ -0,0 +1,17 @@
+extends Spatial
+
+var countdown = 3
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if countdown > 0:
+		countdown -= 1
+		if countdown <= 0:
+			print("hiding shadercache")
+			hide()
