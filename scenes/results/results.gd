0167d8f9f516228a718b08a494f1fb8183a3a9b2
c0linw <41310013+c0linw@users.noreply.github.com>
2022-07-31 17:40:32 -0700

    Fix some defaults and sizing

--- scenes/results/results.gd
+++ scenes/results/results.gd
@@ -4,15 +4,26 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var result_data: Dictionary
-var score: int
+var result_data
+var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/NoteCounts.set_results(SceneSwitcher.get_param("result_data"))
-	$MarginContainer2/SongScore.set_score(SceneSwitcher.get_param("score"))
-	$MarginContainer2/SongScore.set_song_name(SceneSwitcher.get_param("song_title"))
-	$MarginContainer2/SongScore.set_song_difficulty(SceneSwitcher.get_param("difficulty"))
+	result_data = SceneSwitcher.get_param("result_data")
+	if result_data:
+		$MarginContainer/NoteCounts.set_results(result_data)
+		
+	score = SceneSwitcher.get_param("score")
+	if score:
+		$MarginContainer2/SongScore.set_score()
+		
+	var song_title = SceneSwitcher.get_param("song_title")
+	if song_title:
+		$MarginContainer2/SongScore.set_song_name(song_title)
+		
+	var difficulty = SceneSwitcher.get_param("difficulty")
+	if difficulty:
+		$MarginContainer2/SongScore.set_song_difficulty(difficulty)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
