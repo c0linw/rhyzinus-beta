1b00cc55653f7e087ed23d80073479bfecf1d39a
c0linw <41310013+c0linw@users.noreply.github.com>
2022-10-06 11:38:43 -0700

    Commit missing autoload script

---
+++ scripts/shinobu_globals.gd
@@ -0,0 +1,44 @@
+extends Node
+
+enum sfx_enums {SFX_NONE, SFX_CLICK, SFX_SWIPE}
+
+# Declare member variables here. Examples:
+var music_group: ShinobuGroup
+var sfx_group: ShinobuGroup
+
+var sfx_sources: Array
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Shinobu.initialize()
+	
+	sfx_group = Shinobu.create_group("sfx_group", null)
+	music_group = Shinobu.create_group("music", null)
+	
+	# load the note sound effects
+	var click_file = File.new()
+	click_file.open("res://sound/click.wav", File.READ)
+	var buffer = click_file.get_buffer(click_file.get_len())
+	click_file.close()
+	var click_source = Shinobu.register_sound_from_memory("click", buffer)
+	
+	var swipe_file = File.new()
+	swipe_file.open("res://sound/swipe.wav", File.READ)
+	var buffer2 = swipe_file.get_buffer(swipe_file.get_len())
+	swipe_file.close()
+	var swipe_source = Shinobu.register_sound_from_memory("swipe", buffer2)
+	
+	sfx_sources.resize(sfx_enums.size())
+	sfx_sources[sfx_enums.SFX_CLICK] = click_source
+	sfx_sources[sfx_enums.SFX_SWIPE] = swipe_source
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func set_music_volume(linear_volume: float):
+	music_group.set_volume(linear_volume)
+	
+func set_sfx_volume(linear_volume: float):
+	sfx_group.set_volume(linear_volume)
